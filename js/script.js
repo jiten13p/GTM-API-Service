f7d2985d2d54ebc63b1828f329f116d1e7cc1c84
jiten13p <65897082+jiten13p@users.noreply.github.com>
2020-08-19 18:17:15 +0530

    Update script.js
    Updating button element

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ function Success() {
   if (SUCCESS == true) {
     document.getElementById("auth").innerText = "Success";
     document.getElementById("proceed").style.display = "block";
+    ocument.getElementById("auth").style.display = "none";
   }
 }
