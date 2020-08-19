052a0870b543c7206bdd820bb7fb712aac7c1bc4
jiten13p <65897082+jiten13p@users.noreply.github.com>
2020-08-19 20:52:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ function Success() {
   if (SUCCESS == true) {
     document.getElementById("auth").innerText = "Success";
     document.getElementById("proceed").style.display = "block";
-//     document.getElementById("auth").style.display = "none";
+    document.getElementById("auth").style.display = "none";
   }
 }
