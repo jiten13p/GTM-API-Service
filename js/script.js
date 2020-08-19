7343fa8dbd057b0d618061958014a21a568461b2
jiten13p <65897082+jiten13p@users.noreply.github.com>
2020-08-19 20:54:53 +0530

    Update script.js
    Updating Script for auto-execution of Proceed button

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 function Success() {
   if (SUCCESS == true) {
     document.getElementById("auth").innerText = "Success";
-    document.getElementById("proceed").style.display = "block";
+    //document.getElementById("proceed").style.display = "none";
     document.getElementById("auth").style.display = "none";
+    setTimeout(function (){document.getElementById("proceed").click()},200);
   }
 }
