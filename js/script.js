16e98ea3af9c5f666ef70f046db7bbf97ac5966b
jiten13p <65897082+jiten13p@users.noreply.github.com>
2020-08-19 21:11:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ function Success() {
     document.getElementById("auth").innerText = "Success";
     //document.getElementById("proceed").style.display = "none";
     document.getElementById("auth").style.display = "none";
-    setTimeout(function (){document.getElementById("proceed").click()},200);
+    setTimeout(function (){document.getElementById("proceed").click()},50);
   }
 }
